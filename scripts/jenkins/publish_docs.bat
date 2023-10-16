ba9f196220a57012ebab1c75f125248806755686
Afonso Correia <afonsomfcorreia@gmail.com>
2023-10-16 01:15:48 +0100

    Added docs publishing scripts

---
+++ scripts/jenkins/publish_docs.bat
@@ -0,0 +1,24 @@
+@echo off
+
+@REM Get timestamp
+for /f %%x in ('wmic path win32_utctime get /format:list ^| findstr "="') do (
+    set %%x)
+set /a z=(14-100%Month%%%100)/12, y=10000%Year%%%10000-z
+set /a ut=y*365+y/4-y/100+y/400+(153*(100%Month%%%100+12*z-3)+2)/5+Day-719469
+set /a ut=ut*86400+100%Hour%%%100*3600+100%Minute%%%100*60+100%Second%%%100
+
+set "DESTINATION=docs\tsdoc\%ut%"
+
+@REM # Copy artifacts to destination folder
+mkdir %DESTINATION%
+robocopy tsdoc %DESTINATION% /E > NUL
+
+@REM # Add entry on readme file
+echo. >> docs/tsdoc/readme.md
+echo "[Report from timestamp: 1697408068](./1697408068/index.html)" >> docs/tsdoc/readme.md
+
+git add docs/tsdoc/*
+
+git commit -m "[JENKINS] - Publishing New TSDoc At %DESTINATION%"
+
+git push
